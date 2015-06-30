d604fd7545dee4463ea618696862466f1ea4e894
radex <this.is@radex.io>
2015-06-30 15:52:31 +0200

    Backfill some diffToAlign tests
    OMG failures!!!

--- Demo/DiffyTables Tests/DiffyTables_Tests.swift
+++ Demo/DiffyTables Tests/DiffyTables_Tests.swift
@@ -1,6 +1,34 @@
 import UIKit
 import XCTest
 
+extension AlignmentDiffChange: Equatable {}
+
+// This is really disappointing, Swift…
+
+public func == (left: AlignmentDiffChange, right: AlignmentDiffChange) -> Bool {
+    switch left {
+    case .Insertion(pos: let a, len: let b):
+        switch right {
+        case .Insertion(pos: let aa, len: let bb): return a == aa && b == bb
+        default: return false
+        }
+    case .Deletion(pos: let a, len: let b):
+        switch right {
+        case .Deletion(pos: let aa, len: let bb): return a == aa && b == bb
+        default: return false
+        }
+    }
+}
+
+extension AlignmentDiffChange: Printable {
+    public var description: String {
+        switch self {
+        case .Insertion(let pos, let len): return "Insertion(\(pos), \(len))"
+        case .Deletion (let pos, let len): return "Deletion(\(pos), \(len))"
+        }
+    }
+}
+
 class DiffyTables_Tests: XCTestCase {
     func testLongestCommonSubsequence() {
         var a: [Int]
@@ -80,5 +108,59 @@ class DiffyTables_Tests: XCTestCase {
         b = Array("empty bottle")
         
         XCTAssertEqual(longestCommonSubsequence(a, b), Array("emt ole"))
+    }
+    
+    func testDiff() {
+        var a: [Character]
+        var b: [Character]
+        var c: [AlignmentDiffChange]
+        
+        a = Array("abcdef")
+        b = Array("abcdef")
+        c = []
+        
+        XCTAssertEqual(diffToAlign(a, b), c)
+        
+        a = Array("abcdef")
+        b = Array("xabcdef")
+        c = [.Insertion(pos: 0, len: 1)]
+        
+        XCTAssertEqual(diffToAlign(a, b), c)
+        
+        a = Array("abcdef")
+        b = Array("xabcde")
+        c = [.Insertion(pos: 0, len: 1), .Deletion(pos: 6, len: 1)]
+        
+        XCTAssertEqual(diffToAlign(a, b), c)
+        
+        a = Array("abcdef")
+        b = Array("adef")
+        c = [.Deletion(pos: 1, len: 2)]
+        
+        XCTAssertEqual(diffToAlign(a, b), c)
+        
+        a = Array("abcdef")
+        b = Array("acbdef")
+        c = [.Insertion(pos: 1, len: 1), .Deletion(pos: 3, len: 1)]
+        
+        XCTAssertEqual(diffToAlign(a, b), c)
+        
+        a = Array("a")
+        b = Array("")
+        c = [.Deletion(pos: 0, len: 1)]
+        
+        XCTAssertEqual(diffToAlign(a, b), c)
+        
+        a = Array("ab")
+        b = Array("")
+        c = [.Deletion(pos: 0, len: 2)]
+        
+        XCTAssertEqual(diffToAlign(a, b), c)
+        
+        a = Array("")
+        b = Array("ab")
+        c = [.Insertion(pos: 0, len: 2)]
+        
+        XCTAssertEqual(diffToAlign(a, b), c)
     }
 }
